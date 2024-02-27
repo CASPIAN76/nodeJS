d0ccd06eb56bdb4bc9b3753f465db1743e395c42
CASPIAN76 <thakren76@gmail.com>
2024-02-27 19:35:01 +0530

    insert data in database  using inserone()

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 
   const connectDb = require('./dbConnection')
-
+ const insert = require('./insertData')
 
 const main= async()=>{
  
 
   let result =  await connectDb()
   let data = await result.find().toArray()
-  console.log(data)
+  //console.log(data)
 }
 
 
 main()
+
+insert()
+
